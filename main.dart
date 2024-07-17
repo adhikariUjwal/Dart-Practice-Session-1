e75c54f9cf2c9eae6d7b350ac143621ded1efb09
ujwal001 <adhikaridujwal001@gmail.com>
2024-07-18 00:47:52 +0545

    session 1

--- main.dart
+++ main.dart
@@ -1,71 +1,21 @@
-void main() {
-    Name n = Name("Ujwal", "Adhikari");
-    print(n.firstName);
-    
-}
-
-class Name{
-    String firstName;
-    String lastName;
-
-    Name(
-        this.firstName,
-        this.lastName
-    );
-}
-
 class Person{
-    String firstName;
-    String lastName;
-    int age;
+    String _firstName;
+    String _lastName;
+    String _phone;
 
-    Person(this.firstName, this.lastName, this.age);
-}
+    Person(this._firstName, this._lastName, this._phone);
 
-void typeChecking() {
-    var hello = 'Hello';
-    print(hello.runtimeType);
-}
-
-void printType(dynamic d){
-    if (d is int){
-        print("It is an integer.");
-    }
-    if (d is String){
-        print("It is String.");
-    }
-    if (d is bool){
-        print("It is boolean.");
+    toString(){
+        return "${_firstName} ${_lastName} ${_phone}";
     }
 }
 
-void typeInference1() {
-    dynamic x = 1;
-    if (x is int){
-        print("X is Integer.");
-    }
-    return;
+void main(){
+    List<Person> list = [
+        Person("Mark", "Clow", "8403833"),
+        Person("Ma", "Cl", "84038"),
+        Person("Mar", "Clo", "840383"),
+    ];
+
+    print("Not sorted: ${list}");
 }
-
-void loop() {
-    dynamic y = 0;
-    dynamic x = 10;
-    while(y < x){
-        print("Hell0 $y");
-        y = y + 1;
-    }
-}
-
-dynamic multiplyMethod1(int a, int b){
-    return a*b;
-}
-
-int multiplyMethod2(int a, int b) {
-    return a*b;
-}
-
-App() {
-    print("Constructing App");
-}
-
-
